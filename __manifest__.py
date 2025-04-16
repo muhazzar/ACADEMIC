9f254a28b0fe8663ab2e4bafbd1cb8df5f5779c9
muhazzar <muhazzar639@gmail.com>
2025-04-16 16:00:06 +0700

    wizard

--- __manifest__.py
+++ __manifest__.py
@@ -8,12 +8,14 @@
     "description":"""
 Academic Information Day 1""",
     "data":[
+        "security/groups.xml",
         "security/ir.model.access.csv",
         "menu.xml",
         "course.xml",
         "session.xml",
         "attendee.xml",
         "partner.xml",
+        "wizard/create_attendee.xml", 
     ],
     "Installable":True,
     "auto_install":False,
